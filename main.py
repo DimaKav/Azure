9f62251e3f94f2c1f26ba4f816aded4dd17b21cd
Dmitriy <dkav@live.com>
2019-10-08 12:57:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ app = Flask(__name__)
 
 def read_data():
     # Database connection logic
-    server = os.getenv('SERVER')
-    database = os.getenv('DATABASE')
-    username = os.getenv('USERNAME')
-    password = os.getenv('PASSWORD')
-    driver= os.getenv('DRIVER')
+    server = os.environ('SERVER')
+    database = os.environ('DATABASE')
+    username = os.environ('USERNAME')
+    password = os.environ('PASSWORD')
+    driver= os.environ('DRIVER')
     cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
     # Read data from sql connection into a pandas dataframe, output to json
     neg = "SELECT created_at, author, tweet_text, sentiment FROM negative_sentiment"
@@ -22,12 +22,11 @@ def read_data():
     pos = pd.read_sql(pos, cnxn)
     neg = pd.read_sql(neg, cnxn)
     df = pd.concat([pos,neg],0)
-#     return df.sort_values(['sentiment','created_at'])[:10].to_html(index=False)
-    return df.sort_values(['sentiment','created_at'])[:10].to_json(orient='records')
+    return df.sort_values(['sentiment','created_at'])[:10].to_html(index=False)
 
 @app.route("/")
 def hello():
-    return str(read_data())
+    return read_data()
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
